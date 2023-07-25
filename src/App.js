65bbd4c212273b5978f99284a364560e6e132555
yebin <yebind3308@gmail.com>
2023-07-25 21:41:09 +0900

    💄 start, login 레이아웃 구현

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './index.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Start from './pages/Start';
 import Login from './pages/Login';
 import Main from './pages/Main';
-import Record from './pages/ToRecord';
+import ToRecord from './pages/ToRecord';
+import ViewRecord from './pages/ViewRecord';
 import MapSearch from './pages/MapSearch';
-import ExerciseList from './pages/ExerciseList';
+import ExerciseGPS from './pages/ExerciseGPS';
 import Calendar from './pages/Calendar';
 import Settings from './pages/Settings';
 import MyInfo from './pages/MyInfo';
 
 function App() {
+  // Header와 Footer가 보여질 페이지 경로 목록
+  const location = useLocation();
+  const showHeaderFooterPaths = ['/Main', '/Record', '/MapSearch', '/ExerciseList', '/Calendar', '/MyInfo', '/Settings'];
+  // 현재 경로가 showHeaderFooterPaths에 속하는지 확인하는 함수
+  const showHeaderFooter = () => showHeaderFooterPaths.includes(location.pathname);
+
   return (
     <>
-      <Header />
+      {/* 조건부로 Header를 렌더링 */}
+      {showHeaderFooter() && <Header />}
       <Routes>
         <Route path='/' element={<Start />}></Route>
-        <Route path='/Login' element={<Login />}></Route>
-        <Route path='/Main' element={<Main />}></Route>
-        <Route path='/Record' element={<Record />}></Route>
-        <Route path='/MapSearch' element={<MapSearch />}></Route>
-        <Route path='/ExerciseList' element={<ExerciseList />}></Route>
-        <Route path='/Calendar' element={<Calendar />}></Route>
-        <Route path='/MyInfo' element={<MyInfo />}></Route>
-        <Route path='/Settings' element={<Settings />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/main' element={<Main />}></Route>
+        <Route path='/to-record' element={<ToRecord />}></Route>
+        <Route path='/view-record' element={<ViewRecord />}></Route>
+        <Route path='/mapSearch' element={<MapSearch />}></Route>
+        <Route path='/exercise-gps' element={<ExerciseGPS />}></Route>
+        <Route path='/calendar' element={<Calendar />}></Route>
+        <Route path='/myInfo' element={<MyInfo />}></Route>
+        <Route path='/settings' element={<Settings />}></Route>
       </Routes>
-      <Footer />
+      {/* 조건부로 Footer를 렌더링 */}
+      {showHeaderFooter() && <Footer />}
     </>
   );
 }
--- src/pages/ToRecord.js
+++ src/pages/ToRecord.js
@@ -4,8 +4,8 @@ import React from "react";
 
 const ToRecord = () => {
     return (
-        <>
-        </>
+        <main className="w-80 h-[800px] bg-[#FDF6E6] absolute top-0 left-0 z-0">
+        </main>
     )
 };
 
