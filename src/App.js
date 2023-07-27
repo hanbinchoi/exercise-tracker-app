aecddabdb978e9cb3dc63a9369052a56d449206c
yebin <yebind3308@gmail.com>
2023-07-27 21:41:55 +0900

    ✨ 카테고리 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,40 +8,51 @@ import Main from './pages/Main';
 import ToRecord from './pages/ToRecord';
 import ViewRecord from './pages/ViewRecord';
 import MapSearch from './pages/MapSearch';
-import Timer from './pages/Timer';
+import ExerciseGPS from './pages/ExerciseGPS';
 import Calendar from './pages/Calendar';
 import Settings from './pages/Settings';
 import MyInfo from './pages/MyInfo';
 import MapContainer from './components/Map/MapContainer';
 import LandingPage from './components/Map/LandingPage';
-import GoogleLogin from './components/Login/GoogleLogin';
-import KaKaoLogin from './components/Login/KaKaoLogin';
-import Category from './components/Category';
+
 
 function App() {
   // Header와 Footer가 보여질 페이지 경로 목록
   const location = useLocation();
-  const showHeaderFooterPaths = ['/main', '/to-record', '/view-record', '/timer', '/mapSearch', '/calendar', '/myInfo', '/settings'];
+  const showHeaderFooterPaths = [
+    "/main",
+    "/Record",
+    "/MapSearch",
+    "/ExerciseList",
+    "/Calendar",
+    "/MyInfo",
+    "/Settings",
+  ];
   // 현재 경로가 showHeaderFooterPaths에 속하는지 확인하는 함수
-  const showHeaderFooter = () => showHeaderFooterPaths.includes(location.pathname);
+  const showHeaderFooter = () =>
+    showHeaderFooterPaths.includes(location.pathname);
 
   return (
     <>
-
       {/* 조건부로 Header를 렌더링 */}
       {showHeaderFooter() && <Header />}
       <Routes>
+
         <Route path='/' element={<Start />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/main' element={<Main />}></Route>
+        <Route path='/Kakao/callback' element={<Main />}></Route>
+        <Route path='/Oauth' element={<Main />}></Route>
         <Route path='/to-record' element={<ToRecord />}></Route>
         <Route path='/view-record' element={<ViewRecord />}></Route>
+        <Route path='/MapSearch' element={<MapSearch />}></Route>
+        <Route path='/exercise-gps' element={<ExerciseGPS />}></Route>
         <Route path='/calendar' element={<Calendar />}></Route>
-        <Route path='/timer' element={<Timer />}></Route>
-        <Route path='/mapSearch' element={<MapSearch />}></Route>
         <Route path='/myInfo' element={<MyInfo />}></Route>
         <Route path='/settings' element={<Settings />}></Route>
+
       </Routes>
+
       {/* 조건부로 Footer를 렌더링 */}
       {showHeaderFooter() && <Footer />}
     </>
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,47 +1,23 @@
 // 운동 기록 (ver. 캘린더)
 import React, { useState } from "react";
-import CalendarLib from "react-calendar";
+// import "react-calendar/dist/Calendar.css"; // css import
 import moment from "moment";
-import { useSelector } from "react-redux";
-import "./CalendarStyle.css";
-
+import CalendarComponent from "../components/CalendarComponent";
+import SimpleExerciseListView from "../components/SimpleExerciseListView";
+import "./Calendar.css"
 const Calendar = () => {
   // value = 선택한 날짜 / onChange = 날짜 변경 함수
   const [date, setDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
-  const data = useSelector((state) => state.exercise.value);
-
-  // 운동을 추가한 날짜들
-  const mark = [...new Set(data.map((ele) => ele.date))];
-
   const onDateChange = (date) => {
     setDate(moment(date).format("YYYY-MM-DD"));
   };
 
   return (
-    <main className="w-80 h-[800px] bg-[#ffffff] absolute top-0 left-0 z-0 ">
-      {/* 캘린더 라이브러리 */}
-      <div className=" w-[300px] h-[300px] p-[0px] absolute top-[120px] left-[10px]">
-        <CalendarLib
-          // 달 이동 화살표
-          nextLabel={null}
-          prevLabel={null}
-          next2Label={null}
-          prev2Label={null}
-          // 일에 숫자만 들어가기
-          formatDay={(locale, date) =>
-            date.toLocaleString("en", { day: "numeric" })
-          }
-          // 값이 변경 될 때 호출되는 함수
-          onChange={(d) => onDateChange(d)}
-          value={date}
-          tileClassName={({ date, view }) => {
-            if (mark.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
-              return "highlight";
-            }
-          }}
-        />
-      </div>
-    </main>
+    <div className="calendar-page-container">
+      <CalendarComponent date={date} onDateChange={onDateChange} />
+      <hr />
+      <SimpleExerciseListView date={date} />
+    </div>
   );
 };
 
