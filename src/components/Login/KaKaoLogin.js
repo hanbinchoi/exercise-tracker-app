dee347cf9c38162e6e4f250b05921ced3978fd55
hanbinchoi <wkghskak@naver.com>
2023-08-08 22:19:05 +0900

    api 환경변수 수정

--- src/components/Login/KaKaoLogin.js
+++ src/components/Login/KaKaoLogin.js
@@ -3,26 +3,23 @@ import KakaoUserInfo from "./KaKaoUserInfo";
 const SocialKAKAO = () => {
   // API, URI 받아 오기
   // URL 설정
-  const CLIENT_ID = `${process.env.REACT_APP_REST_API_KEY}`
-  const REDIRECT_URI = `${process.env.REACT_APP_REST_API_URI}`
+  const CLIENT_ID = `${process.env.REACT_APP_REST_API_KEY}`;
+  const REDIRECT_URI = `${process.env.REACT_APP_REST_API_URI}`;
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   // 로그인 버튼을 눌렀을 때 해당 링크로 이동하게끔 설정
   const handleLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
-  }
+  };
 
   return (
     <>
-      <button
-        className="text-xl font-bold mx-5"
-        onClick={handleLogin}
-      >
-        카카오 로그인</button>
+      <button className="text-xl font-bold mx-5" onClick={handleLogin}>
+        카카오 로그인
+      </button>
       <KakaoUserInfo />
     </>
   );
 };
-
 
 export default SocialKAKAO;
--- src/components/Login/KaKaoUserInfo.js
+++ src/components/Login/KaKaoUserInfo.js
@@ -1,54 +1,54 @@
-import { useEffect,} from "react";
-import axios from "axios"
+import { useEffect } from "react";
+import axios from "axios";
 
-const KakaoUserInfo = ({isLogin,setIsLogin}) => {
-    // const [userName, setUserName] = useState();
-    useEffect(() => {
-        
+const KakaoUserInfo = ({ isLogin, setIsLogin }) => {
+  // const [userName, setUserName] = useState();
+  useEffect(() => {
+    const params = new URL(document.location.toString()).searchParams;
+    const code = params.get("code");
+    const grantType = "authorization_code";
+    const CLIENT_ID = `${process.env.REACT_APP_REST_API_KEY}`;
+    const REDIRECT_URI = `${process.env.REACT_APP_REST_API_URI}`;
 
-        const params = new URL(document.location.toString()).searchParams;
-        const code = params.get('code');
-        const grantType = "authorization_code";
-        const CLIENT_ID = `${process.env.REACT_APP_REST_API_KEY}`
-        const REDIRECT_URI = `${process.env.REACT_APP_REST_API_URI}`
+    axios
+      .post(
+        `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+        {},
+        {
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
 
-        axios.post(
-            `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+        const { access_token } = res.data;
+        axios
+          .post(
+            `https://kapi.kakao.com/v2/user/me`,
             {},
-            { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }
-        )
-            .then((res) => {
-                console.log(res);
+            {
+              headers: {
+                Authorization: `Bearer ${access_token}`,
+                "Content-type":
+                  "application/x-www-form-urlencoded;charset=utf-8",
+              },
+            }
+          )
 
+          .then((res) => {
+            console.log("2번째", res);
+            setIsLogin(true);
+            console.log(res.data.properties.nickname);
+            sessionStorage.setItem("username", res.data.properties.nickname);
+          });
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  }, [setIsLogin]);
 
-                const { access_token } = res.data;
-                axios.post(
-                    `https://kapi.kakao.com/v2/user/me`,
-                    {},
-                    {
-                        headers: {
-                            Authorization: `Bearer ${access_token}`,
-                            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-                        }
-                    }
-                )
-
-                    .then((res) => {
-                        console.log('2번째', res);
-                        setIsLogin(true);
-                        console.log(res.data.properties.nickname)
-                        sessionStorage.setItem('username',res.data.properties.nickname); 
-                        
-                    })
-            })
-            .catch((Error) => {
-                console.log(Error)
-            })
-    }, [setIsLogin])
-
-    return (
-        <>
-        </>
-    )
-}
+  return <></>;
+};
 export default KakaoUserInfo;
