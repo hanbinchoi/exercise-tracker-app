eb7004255eba5f00238c5e4ebb888e13cdb07291
yebin <yebind3308@gmail.com>
2023-08-08 20:34:36 +0900

    🚀 배포(3)

--- src/pages/MapSearch.js
+++ src/pages/MapSearch.js
@@ -1,13 +1,12 @@
 // 주변 헬스장 정보 받아오기 창
-
 import React from "react";
-import LandingPage from "../components/Map/LandingPage";
+// import LandingPage from "../components/Map/LandingPage";
 
 const MapSearch = () => {
     return (
         <main className="w-80 h-[800px] bg-[#ffffff] absolute top-0 left-0 border-2 border-[#E6E6E6] z-0 flex justify-center">
             <div className=" absolute top-[80px] box-border">
-                <LandingPage />
+                {/* <LandingPage /> */}
             </div>
 
         </main>
